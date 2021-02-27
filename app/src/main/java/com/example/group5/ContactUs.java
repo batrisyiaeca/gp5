99c1d9e1c6c0f0c78176efc81e6c52cb0adb45db
Ainsley69 <alarafeenalsaufeen@gmail.com>
2021-02-27 16:40:33 +0800

    help me

--- app/src/main/java/com/example/group5/ContactUs.java
+++ app/src/main/java/com/example/group5/ContactUs.java
@@ -3,6 +3,7 @@ package com.example.group5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
