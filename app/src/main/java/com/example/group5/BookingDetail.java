a960fba175985b64f422ca9f0f79d62c7d49e13b
Ainsley69 <alarafeenalsaufeen@gmail.com>
2021-03-03 13:43:49 +0800

    help me

---
+++ app/src/main/java/com/example/group5/BookingDetail.java
@@ -0,0 +1,58 @@
+package com.example.group5;
+
+public class BookingDetail {
+
+    String name, email, phone , time, date;
+
+    public BookingDetail(){
+
+    }
+
+    public BookingDetail(String name, String email, String phone, String time, String date){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.time = time;
+        this.date = date;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
