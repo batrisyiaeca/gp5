ad6093b09936b25a8b7105d712f06438c41627e9
Ainsley69 <alarafeenalsaufeen@gmail.com>
2021-03-09 22:21:05 +0800

    help me

--- app/src/main/java/com/example/group5/SecondActivity.java
+++ app/src/main/java/com/example/group5/SecondActivity.java
@@ -17,6 +17,7 @@ public class SecondActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private CardView logout;
+    private CardView profile, contactUs, viewRoom, viewCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,11 @@ public class SecondActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         logout = (CardView) findViewById(R.id.cv_logout);
+        profile = (CardView) findViewById(R.id.cv_profile);
+        contactUs = (CardView) findViewById(R.id.cv_contactus);
+        viewRoom = (CardView) findViewById(R.id.cv_viewroom);
+        viewCart = (CardView) findViewById(R.id.cv_cart);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +39,34 @@ public class SecondActivity extends AppCompatActivity {
                 Logout();
 
 
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this, ProfileActivity.class));
+            }
+        });
+
+        contactUs.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(SecondActivity.this, ContactUs.class));
+            }
+        });
+
+        viewRoom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(SecondActivity.this, ViewRoom.class));
+            }
+        });
+
+        viewCart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(SecondActivity.this, CartActivity.class));
             }
         });
     }
